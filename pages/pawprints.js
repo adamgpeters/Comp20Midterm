dd45738ec85e8662bfa6bf88d1f6ba53f5d16d24
adamgpeters <petersadam18@gmail.com>
2020-10-26 16:18:39 -0400

    style fixed

--- pages/pawprints.js
+++ pages/pawprints.js
@@ -3,32 +3,30 @@ for (let i = 0; i < 10; i++) {
     $('#pawprints').append('<img class="paw" id="paw-' + i + '" style="transition: .3s;opacity:0;position: absolute;width: 5%;" src="../assets/pawprint.png" />');
 }
 
-var w = $('#pawprints').width();
-var h = $('#pawprints').height();
+var containerWidth = $('#pawprints').width();
+var containerHeight = $('#pawprints').height();
 $('.paw').each(function (i) {
-    let vertOffset = Math.random() * h / 4 + h / 8;
-    var func = 0;
+    let vertOffset = Math.random() * containerHeight / 4 + containerHeight / 8;
+    var legOffset = 0;
     var rotate = 90;
     if (i < 4) {
-        func = -1 * i * h / 6 + h / 6;
+        legOffset = -1 * i * containerHeight / 6 + containerHeight / 6;
         rotate = Math.random() * 30 + 90;
     } else if (i > 6) {
-        func = -1 * (i - 3) * h / 6 + h / 6;
+        legOffset = -1 * (i - 3) * containerHeight / 6 + containerHeight / 6;
         rotate = Math.random() * 30 + 90;
     } else {
         rotate = Math.random() * 30 + 75;
     }
 
-
-
     $(this).css({ 'transform': 'rotate(' + rotate + "deg)" });
     if (i % 2 == 0) {
-        $(this).css({ 'top': '' + (h / 2.0 - vertOffset - func) + "px" });
+        $(this).css({ 'top': '' + (containerHeight / 2.0 - vertOffset - legOffset) + "px" });
     } else {
-        $(this).css({ 'top': '' + (h / 2.0 + vertOffset - func) + "px" });
+        $(this).css({ 'top': '' + (containerHeight / 2.0 + vertOffset - legOffset) + "px" });
     }
 
-    $(this).css({ 'left': '' + (w / 10.0 * i + $(this).width() / 2.0) + "px" });
+    $(this).css({ 'left': '' + (containerWidth / 10.0 * i + $(this).width() / 2.0) + "px" });
 });
 
 function showPrints() {
@@ -40,9 +38,6 @@ function showPrints() {
 }
 
 $(document).ready(function () {
-    // hideIfBelow();
-    /* Every time the window is scrolled ... */
-
 
     $(window).scroll(function () {
         var bottom_of_window = $(window).scrollTop() + $(window).height();
@@ -50,5 +45,4 @@ $(document).ready(function () {
             showPrints();
         }
     });
-
 });
